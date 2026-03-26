2c6af42caa631a09af50d1fdf6a6902f6f6936b1
Vivek Marathe <vivekmarathe034@gmail.com>
2026-03-26 15:44:35 +0530

    Delete Online Food directory

--- Online Food/config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-require_once __DIR__ . '/db.php';
-require_once __DIR__ . '/../includes/functions.php';
-require_once __DIR__ . '/../includes/auth.php';
-?>
